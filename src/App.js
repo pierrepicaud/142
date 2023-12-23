ca9273d4e3f5a62f0f99188d77f12cd5e224a9ac
Pierre Picaud <pierrepicaud385@gmail.com>
2023-12-24 01:23:08 +0300

    Add onlick randomize background picture with transition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Snowfall from 'react-snowfall';
 
 function App() {
+  const images = [
+    './assets/christmas1.png',
+    './assets/christmas2.png',
+    './assets/christmas3.png',
+    './assets/christmas4.png',
+    './assets/christmas5.png',
+    './assets/christmas6.png',
+    './assets/christmas7.png',
+    './assets/christmas8.png',
+  ];
+
+  const getRandomImage = () => images[Math.floor(Math.random() * images.length)];
+
+  // State for the background image
+  const [backgroundImage, setBackgroundImage] = useState(getRandomImage());
+
+  // Event handler for changing the background
+  const changeBackground = () => {
+    setBackgroundImage(getRandomImage());
+  };
+
+  // Inline style for background image
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={backgroundStyle} onClick={changeBackground}>
       <Snowfall />
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
+        <p className='gvs'>
           Merry Christmas and 143 pặc pặc!
         </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
