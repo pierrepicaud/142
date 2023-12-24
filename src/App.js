4e23792b4f805799242b7655e9214874b5a1d376
Pierre Picaud <pierrepicaud385@gmail.com>
2023-12-24 10:36:05 +0300

    Add sound effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Snowfall from 'react-snowfall';
+import useSound from 'use-sound';
 
 function App() {
+  const soundUrl = process.env.PUBLIC_URL + '/assets/AllIWant.mp3';
+  const [play] = useSound(soundUrl, { volume: 0.5 });
   const images = [
     'assets/christmas1.png',
     'assets/christmas2.png',
@@ -32,9 +35,13 @@ function App() {
     backgroundPosition: 'center',
     // 'background-color': '#34302c'
   };
+  
+  useEffect(() => {
+    play();
+  }, [play])
 
   return (
-    <div className="App" style={backgroundStyle} onClick={changeBackground}>
+    <div className="App" style={backgroundStyle} onClick={changeBackground} >
       <Snowfall />
       <header className="App-header">
         <p className='gvs'>
